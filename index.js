d28817689f25f8b6893d31599375513c93e9b9e3
Kevin Griffin <kgriffin@meditech.com>
2023-09-20 04:52:15 -0400

    wip

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+// const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const fs = require("fs");
 
-function processChild(ObjIn) {
-    const jsonObj = ObjIn.jsonObj;
-    const haploCsvPath = ObjIn.haploCsvPath;
-    const child = ObjIn.child;
+function processChild(objIn) {
+    const jsonObj = objIn.jsonObj;
+    const haploCsvPath = objIn.haploCsvPath;
+    const child = objIn.child;
     const csvDelim = String.fromCharCode(44);
     const dblQuote = String.fromCharCode(34);
 
@@ -33,23 +33,87 @@ function processChild(ObjIn) {
     }
 }
 
-function processChildren(jsonObj, childNodes) {
+function processSubBranches(objIn) {
+    const jsonObj = objIn.jsonObj;
+    const parent = objIn.parent;
+    const path = objIn.path;
     const csvDelim = String.fromCharCode(44);
     const dblQuote = String.fromCharCode(34);
+
+    if (parent?.subBranches > 1000) {
+        parent.children.forEach(child => {
+            processSubBranches(
+                {
+                    jsonObj,
+                    "parent": jsonObj.allNodes[child],
+                    "path": path + "\\" + parent.name + "\\" + jsonObj.allNodes[child].name
+                });
+        });
+    } else {
+
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path, { recursive: true });
+        }
+
+        fs.appendFileSync(
+            path + "\\" + parent.name + ".csv",
+            "tMRCA(BCE/CE)" + csvDelim
+            + "kits" + csvDelim
+            + "root" + csvDelim
+            + "variants" + csvDelim
+            + "cumulative variants" + csvDelim
+            + "tree" + "\n"
+        );
+
+        parent.children
+            ?.filter(child => String(child).match(/\d+/))
+            .forEach(child => {
+                fs.appendFileSync(
+                    path + "\\" + parent.name + ".csv",
+                    "N/A" + csvDelim
+                    + jsonObj.allNodes[child].subBranches + csvDelim
+                    + "N/A" + csvDelim
+                    + dblQuote
+                    + jsonObj.allNodes[child].variants.reduce(
+                        (prev, cur, idx) => prev + (idx === 0 ? "" : ",") + cur.variant,
+                        "")
+                    + dblQuote + csvDelim
+                    + "N/A" + csvDelim
+                    + jsonObj.allNodes[child].name + "\n"
+                );
+            });
+
+        return;
+    }
+}
+
+function processChildren(jsonObj, childNodes, parentName) {
+    const csvDelim = String.fromCharCode(44);
     let haploName = "";
     let haploCsvPath = "";
+
+    if (!fs.existsSync("output\\" + parentName)) {
+        fs.mkdirSync("output\\" + parentName);
+    }
 
     childNodes.forEach((node) => {
 
         if (jsonObj.allNodes[node].subBranches > 1000) {
             // recurse
+            processSubBranches(
+                {
+                    jsonObj,
+                    parent: jsonObj.allNodes[node],
+                    path: "output\\" + parentName + "\\" + jsonObj.allNodes[node].name
+                }
+            );
         } else {
             haploName = jsonObj.allNodes[node].name;
-            haploCsvPath = "output/" + haploName + ".csv";
+            haploCsvPath = "output\\" + parentName + "\\" + haploName + ".csv";
 
-            if (fs.existsSync(haploCsvPath)) {
-                fs.unlinkSync(haploCsvPath);
-            }
+            // if (fs.existsSync(haploCsvPath)) {
+            //     fs.unlinkSync(haploCsvPath);
+            // }
 
             fs.appendFileSync(
                 haploCsvPath,
@@ -82,9 +146,18 @@ function processJSON() {
     const csvDelim = String.fromCharCode(44);
     const dblQuote = String.fromCharCode(34);
 
-    if (fs.existsSync(rootsCsvPath)) {
-        fs.unlinkSync(rootsCsvPath);
+    if (fs.existsSync("output")) {
+        fs.rmSync("output", { recursive: true, force: true });
+        fs.mkdirSync("output");
     }
+
+    // if (!fs.existsSync("output")) {
+    //     fs.mkdirSync("output");
+    // }
+
+    // if (fs.existsSync(rootsCsvPath)) {
+    //     fs.unlinkSync(rootsCsvPath);
+    // }
 
     fs.appendFileSync(
         rootsCsvPath,
@@ -96,7 +169,8 @@ function processJSON() {
         + "tree" + "\n"
     );
 
-    Object.keys(jsonObj.roots).forEach((key) => {
+    Object.keys(jsonObj.roots).forEach((key, idx) => {
+        if (idx === 0) { return; } // skip redundant entry
         fs.appendFileSync(
             rootsCsvPath,
             "N/A" + csvDelim
@@ -110,7 +184,7 @@ function processJSON() {
             + "N/A" + csvDelim
             + jsonObj.roots[key].name + "\n"
         );
-        processChildren(jsonObj, jsonObj.roots[key].children);
+        processChildren(jsonObj, jsonObj.roots[key].children, jsonObj.roots[key].name);
     });
 
     console.log("done");
